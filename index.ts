ab5658a0127bc6e531dcfb93c29cf242e038c3e7
roadisfirst <roadisfirst@gmail.com>
2021-08-07 16:53:57 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -6,36 +6,36 @@ enum Fruit {
   APPLE = 'apple'
 }
 
-type fruitBasket = { [key in Fruit]: number };
+type FruitBasket = { [key in Fruit]: number };
 
-const fruitBasket: fruitBasket = {
-  banana: 2,
-  orange: 3,
-  kiwi: 2,
-  apple: 3
+const fruitBasket: FruitBasket = {
+  [Fruit.BANANA]: 2,
+  [Fruit.ORANGE]: 3,
+  [Fruit.KIWI]: 2,
+  [Fruit.APPLE]: 3
 };
 
 // 2. Add typings/access modifiers to the Person class
+enum Gender {
+  MALE = 'male',
+  FEMALE = 'female'
+}
+type GenderType = [Gender.MALE] | [Gender.FEMALE];
+
 class Person {
-  name: string;
-  gender: string;
-  age: number;
-  likes: string[];
   public constructor(
-    name: string,
-    gender: string,
-    age: number,
-    likes: string[]
-  ) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.likes = likes;
-  }
+    public name: string,
+    public gender: GenderType,
+    public age: number,
+    public likes: string[]
+  ) {}
 
   public introduce(this: Person): string {
     const { name, gender, age, likes } = this;
-    const goodLookingMap = new Map([['male', 'handsome'], ['female', 'cute']]);
+    const goodLookingMap = new Map([
+      [[Gender.MALE], 'handsome'],
+      [[Gender.FEMALE], 'cute']
+    ]);
     return `
       Hello, I'm ${name}, ${age} years old, I like: ${likes.join(', ')}. 
       As you can see, I'm quite ${goodLookingMap.get(gender)} too!
@@ -43,7 +43,10 @@ class Person {
   }
 }
 
-const Dima = new Person('Dima', 'male', 22, ['video games', 'martial arts']);
+const Dima = new Person('Dima', [Gender.MALE], 22, [
+  'video games',
+  'martial arts'
+]);
 
 // 3. Add typings/access modifiers to MovieService class
 
@@ -60,16 +63,16 @@ class MovieService {
   }
 }
 
-abstract class Logger {
-  abstract log(err: Error): void;
+interface Logger {
+  log(err: Error): void;
 }
 
-class LoggerOne extends Logger {
+class LoggerOne implements Logger {
   public log(err: Error): void {
     console.log('sending logs to log storage 1', err);
   }
 }
-class LoggerTwo extends Logger {
+class LoggerTwo implements Logger {
   public log(err: Error): void {
     console.log('sending logs to log storage 2', err);
   }
